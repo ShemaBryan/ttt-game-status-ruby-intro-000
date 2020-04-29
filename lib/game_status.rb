809f99f8ee56b83a44261e657d6a2e7359a7d778
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-04-29 19:25:09 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -13,7 +13,7 @@ def won?(board)
  WIN_COMBINATIONS.detect do |win_combination|
   board[win_combination[0]] == board[win_combination[1]] &&
   board[win_combination[1]] == board[win_combination[2]] &&
-  position_taken?(board, win_combination[0]])
+  position_taken?(board, win_combination[0])
  end
  # WIN_COMBINATIONS.each do |win_combination|
  #   win_index_1 = win_combination[0]
